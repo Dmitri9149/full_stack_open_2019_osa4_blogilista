f6c9ccdedb35485ae999a329d1d1230a80cad596
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-05-18 16:34:15 +0300

    4.6

--- tests/list.test.js
+++ tests/list.test.js
@@ -187,3 +187,101 @@ describe('favorite blog', () => {
 
 })
 
+
+
+describe('most blogs', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const theOnlyElem = {
+    author: 'Edsger W. Dijkstra',
+    blogs: 1
+  }
+
+  const emptyBlog = []
+
+  const bigBlog = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }
+  ]
+
+  const elemFromBigBlog = {
+    author: "Robert C. Martin",
+    blogs: 3
+  }
+
+  test('of empty list is empty', () => {
+    const result = listHelper.mostBlogs(emptyBlog)
+    expect(result).toEqual({})
+  })
+
+
+
+  test('when list has only one blog equals the object with same author and blogs = 1', () => {
+    const result = listHelper.mostBlogs(listWithOneBlog)
+    expect(result).toEqual(theOnlyElem)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.mostBlogs(bigBlog)
+    expect(result).toEqual(elemFromBigBlog)
+  })
+
+})
+
+
+
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -33,18 +33,36 @@ const favoriteBlog = blogs => {
 
 }
 
-const mostBlogs = blogs => {
-  const collectionA = _.groupBY(blogs, author)
-  const collectionB = _.forEach(collectionA, totalLikes(value))
-  const collectionC = _.sortBy(collectionC, [_.identity])
-  return {
-    author:collecionC[0].
+const favoriteBlog2 = blogs => {
+  const reducer = (sum, blog) => {
+    if(sum[1] < blog[1]) {
+      return blog
+    } else {
+      return sum
+    }
   }
+
+  return blogs.length === 0
+    ? {}
+    : {
+      author:blogs.reduce(reducer, blogs[0])[0],
+      blogs:blogs.reduce(reducer, blogs[0])[1]
+    }
+
+}
+
+const mostBlogs = blogs => {
+  const collectionA = _.groupBy(blogs, function(o) {return o.author})
+  console.log('collectionA', collectionA)
+  const collectionB = _.map(collectionA, function(value, key){return [ key, value.length ]  } )
+  console.log('collectionB', collectionB)
+  return favoriteBlog2 (collectionB)
 }
 
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
