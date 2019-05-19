a374474ea1f7d22df7c7b363178cd3208dc91a84
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-05-19 18:20:37 +0300

    initial commit to jest.config.js + some changes in config.js like cons.logs

--- config.js
+++ config.js
@@ -7,6 +7,9 @@ if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI
 }
 
+console.log('NODE_ENV = ', process.env.NODE_ENV)
+console.log()
+
 module.exports = {
   MONGODB_URI,
   PORT
