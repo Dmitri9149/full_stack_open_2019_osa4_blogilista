b188b8e3898aa559fc29ea70ef8c3e52bafe29f5
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-05-20 20:10:56 +0300

    beforeEach is added

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,43 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('models/blog')
+
+const initialBlogs = [
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url:'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5
+  },
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7
+  },
+  {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10
+
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+
+  blogObject = new Blog(initialBlogs[2])
+  await blogObject.save()
+})
 
 test('notes are returned as json', async () => {
   await api
