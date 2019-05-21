ba0b85d37e1a6b89a2d6eafff412fe9c282503b5
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-05-21 19:10:51 +0300

    test work if expect(201) code

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -70,6 +70,32 @@ test('identification field has name as id ', async () => {
 
 })
 
+test('a valid note can be added ', async () => {
+
+  const newBlog = {
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2
+  }
+
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(r => r.title)
+
+  expect(response.body.length).toBe(initialBlogs.length + 1)
+  expect(titles).toContain(
+    'Type wars'
+  )
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
