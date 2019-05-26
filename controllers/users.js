35e09fda3b2dd0d2a45d5f53d44f6af3c848ea6f
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-05-26 19:57:35 +0300

    conditions for passwords are added

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,9 @@ const User = require('../models/user')
 usersRouter.post('/', async (request, response, next) => {
   try {
     const body = request.body
+    if (body.password === undefined || body.password.length < 3) {
+      return response.status(400).json({ error: 'content missing' })
+    }
 
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(body.password, saltRounds)
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -227,6 +227,7 @@ describe('when there is initially one user at db', () => {
     const user = new User ({
       username:'root',
       name:'superuser',
+      password:'sekret',
       passwordHash
     })
     await user.save()
