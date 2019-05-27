76555bc5d723587239861a0bd8c904e212179602
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-05-27 13:23:01 +0300

    field user: is added to blogSchema

--- models/blog.js
+++ models/blog.js
@@ -11,7 +11,11 @@ const blogSchema = new mongoose.Schema({
     type:String,
     requaried:true
   },
-  likes: Number
+  likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 blogSchema.set('toJSON', {
