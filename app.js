5569124f5075b6780140ece262b78d7eb8aa4236
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-05-27 19:54:11 +0300

    ..app.use('(api/logins...) + require('./controllers/login')

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.set('useCreateIndex', true)
 
@@ -25,6 +26,7 @@ app.use(bodyParser.json())
 
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
