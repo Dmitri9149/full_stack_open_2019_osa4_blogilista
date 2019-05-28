2491da22081ca34568d10b9adfa01a36bcef4d94
Dmitri <34885758+Dmitri9149@users.noreply.github.com>
2019-05-28 11:40:35 +0300

    token handling/verification

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
 
 mongoose.set('useFindAndModify', false)
 
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 
 blogsRouter.get('/', async (request, response) => {
@@ -18,17 +27,27 @@ blogsRouter.post('/', async (request, response, next) => {
 
 /*  const user = await User.findById(body.userId)  */
 
-  const users = await User.find({})
-  const user = users[0]
+/*  const users = await User.find({})
+  const user = users[0]       */
 
-  const blog = new Blog({
-    title:body.title,
-    author:body.author,
-    url:body.url,
-    likes:body.likes === undefined ? 0 : body.likes,
-    user:user._id
-  })
+  const token = getTokenFrom(request)
+
   try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+
+    const blog = new Blog({
+      title:body.title,
+      author:body.author,
+      url:body.url,
+      likes:body.likes === undefined ? 0 : body.likes,
+      user:user._id
+    })
 
     if (blog.title === undefined && blog.url === undefined) {
       await response.status(400).end()
